7baae71662e8b7d98af5783e640b9ce6848ce5c6
gcz <saadmin@126.com>
2022-04-18 16:49:50 +0800

    分页封装

--- src/views/search.vue
+++ src/views/search.vue
@@ -51,19 +51,7 @@
             </div>
         </div>
         <!-- 歌手列表 -->
-        <nav class="pagination is-centered" role="navigation" aria-label="pagination">
-            <a class="pagination-previous">上一页</a>
-            <a class="pagination-next">下一页</a>
-            <ul class="pagination-list">
-              <li><a class="pagination-link" aria-label="Goto page 1">1</a></li>
-              <li><span class="pagination-ellipsis">&hellip;</span></li>
-              <li><a class="pagination-link" aria-label="Goto page 45">45</a></li>
-              <li><a class="pagination-link is-current" aria-label="Page 46" aria-current="page">46</a></li>
-              <li><a class="pagination-link" aria-label="Goto page 47">47</a></li>
-              <li><span class="pagination-ellipsis">&hellip;</span></li>
-              <li><a class="pagination-link" aria-label="Goto page 86">86</a></li>
-            </ul>
-          </nav>
+        <pagination :urlPrefix="'/'" :currentPage="2" :lastPage="100" />
         <Empty v-if="albumList.length<=0&&songList.length<=0&&singerList.length<=0" description="没有数据" />
     </section>
     <section class="plyr-wrap">
@@ -83,10 +71,12 @@
 
 <script>
 import Empty from '../components/empty';
+import Pagination from '../components/pagination'
 export default {
   name: "",
   components: {
-    Empty
+    Empty,
+    Pagination
   },
   data() {
     return {
