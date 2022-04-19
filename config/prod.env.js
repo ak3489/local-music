d8d4553aad81c4dec40c70676cb9e343042e189f
gcz <saadmin@126.com>
2022-04-19 09:23:08 +0800

    production

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-07 16:42:39
+ * @LastEditTime: 2022-04-19 09:15:08
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \music-front\config\prod.env.js
+ */
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  BASEAPI: '"http://127.0.0.1:3000"'
 }
