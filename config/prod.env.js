7e93bc1d0302e5de5ae2824d19146fae1443702f
gcz <saadmin@126.com>
2022-04-28 16:55:37 +0800

    auto

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,13 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2022-04-07 16:42:39
- * @LastEditTime: 2022-04-19 09:23:50
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-04-28 16:22:26
+ * @LastEditors: gcz
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \music-front\config\prod.env.js
  */
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASEAPI: '"http://101.34.210.222:3000"'
+  BASEAPI: '"http://101.34.210.222:3000"',
+  BASEURL: '"http://101.34.210.222:3000"'
 }
