e62f0e4331cd0dd282716e4b12e6bd247450fc3f
gcz <saadmin@126.com>
2022-04-28 17:25:51 +0800

    fixbug

--- src/views/search.vue
+++ src/views/search.vue
@@ -13,7 +13,7 @@
     <section class="container">
         <nav class="breadcrumb" aria-label="breadcrumbs">
             <ul>
-                <li @click="typeClick(index)" v-for="(item, index) in searchType" :class="{'is-active':index == searchTypeIndex}" :key="index"><a href="#">{{item}}</a></li>
+                <li @click="typeClick(index)" v-for="(item, index) in searchType" :class="{'is-active':index == searchTypeIndex}" :key="index">{{item}}</li>
             </ul>
         </nav>
         <!-- 歌曲列表 -->
